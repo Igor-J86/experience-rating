f58b0598adb1526518c4f20680c79f10ec5816d0
Igor Jorganovic <igor.jorganovic@bring.com>
2025-03-24 14:37:59 +0100

    Add average in summary

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -9,16 +9,31 @@ type SummaryProps = {
 }
 
 const Summary = ({ scores, selectedArea }:SummaryProps) => {
+  let scoreAverage = 0
+  scores.map((topic) => (
+    scoreAverage = scoreAverage + topic.score
+  ))
+  scoreAverage = scoreAverage / scores.length
   return (
     <div className="summary">
       <h2>{selectedArea} summary</h2>
-      <dl>
-        {scores.map((topic) => (
-          <div className="flex gas" key={topic.name}>
-            <dt>{topic.label}</dt> <dd>{topic.score}</dd>
-          </div>
-        ))}
-      </dl>
+      <div className="flex gal">
+        <dl>
+          {scores.map((topic) => (
+            <div className="flex gas" key={topic.name}>
+              <dt>{topic.label}</dt> <dd>{topic.score}</dd>
+            </div>
+          ))}
+        </dl>
+      {scoreAverage !== 0 &&
+        <div className="average">
+          <h3>Snitt:</h3>
+          <span>
+            {scoreAverage}
+          </span>
+        </div>
+      }
+      </div>
     </div>
   )
 }
